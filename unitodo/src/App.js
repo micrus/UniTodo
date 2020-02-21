baafe57859bd9f5e8e910ff91e5794ac8ffe4da2
micrus <michele.russo@studenti.unicam.it>
2020-02-21 18:20:58 +0100

    Aggiunta navbar e sidedrawer

--- unitodo/src/App.js
+++ unitodo/src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import Layout from './hoc/layout/Layout';
+import Home from './containers/Home/Home';
+import Todos from './containers/Todos/Todos';
 
 const App = () => {
-    return <div>prova</div>;
-}
+  return (
+    <Router>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/todos" component={Todos} />
+        <Redirect to="/" />
+      </Switch>
+    </Layout>
+    </Router>
+  );
+};
 
 export default App;
---
+++ unitodo/src/containers/Todos/Todos.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Todos = () => {
+  return <div>Qui andremo a mettere i nostri todo</div>;
+};
+
+export default Todos;
