c5fba999c6cb2343344677c83b650f596baf2104
micrus <michele.russo@studenti.unicam.it>
2020-02-23 17:31:42 +0100

    aggiunte form formik & yup per login e sign up

--- unitodo/src/App.js
+++ unitodo/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Layout from './hoc/layout/Layout';
 import Home from './containers/Home/Home';
+import Login from './containers/Auth/Login/Login';
+import SignUp from './containers/Auth/SignUp/SignUp';
 import Todos from './containers/Todos/Todos';
 
 const App = () => {
@@ -12,7 +14,8 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/todos" component={Todos} />
-        <Redirect to="/" />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
       </Switch>
     </Layout>
     </Router>
--- unitodo/src/hoc/layout/Layout.js
+++ unitodo/src/hoc/layout/Layout.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Navbar from '../../components/Navigation/Navbar/Navbar'
+import Navbar from '../../components/Navigation/Navbar/Navbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 const MainWrapper = styled.main`
   width: 100%;
   min-height: calc(100vh - 6rem);
   margin-top: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Layout = ({ children }) => (
