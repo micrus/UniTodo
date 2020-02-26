aa9025f6d19632d12baa972bc3edbfbd61966bcf
luca-fuligni <luca.fuligni@studenti.unicam.it>
2020-02-26 16:19:00 +0100

    fetching dei todo

--- unitodo/src/containers/Todos/Todos.js
+++ unitodo/src/containers/Todos/Todos.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
 import styled from "styled-components";
+import { firestoreConnect } from 'react-redux-firebase';
 
 import Heading from "../../components/UI/Headings/Heading";
 import { Container } from "../../hoc/layout/elements";
-import AddTodo from './AddTodo/AddTodo';
-
+import AddTodo from "./AddTodo/AddTodo";
+import Loader from "../../components/UI/Loader/Loader";
+import Todo from "./Todo/Todo";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -21,22 +25,68 @@ const InnerWrapper = styled.div`
   padding: 5rem 4rem;
 `;
 
-const Todos = () => {
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 60rem;
+  flex-direction: column;
+  margin-top: 2rem;
+`;
+
+const Todos = ({ todos, requesting, requested, userId }) => {
+  let content;
+  if (!todos) {
+    content = (
+      <Content>
+        <Loader isWhite />
+      </Content>
+    );
+  } else if (!todos[userId] && requested[`todos/${userId}`]) {
+    content = (
+      <Content>
+        <Heading color="white" size="h2">
+          Non hai alcun todo!
+        </Heading>
+      </Content>
+    );
+  } else {
+    content = (
+      <Content>
+        {todos[userId].todos.map(todo => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </Content>
+    );
+  }
   return (
     <Wrapper>
       <Container>
         <InnerWrapper>
           <Heading noMargin size="h1" color="white">
-            I tuoi Todo
+            Your Todos
           </Heading>
           <Heading bold size="h4" color="white">
-            Tutto quello che devi fare...
+            Tutto quello che devi fare per adesso...
           </Heading>
           <AddTodo />
+          {content}
         </InnerWrapper>
       </Container>
     </Wrapper>
   );
 };
 
-export default Todos;
+const mapStateToProps = ({ firebase, firestore }) => ({
+  userId: firebase.auth.uid,
+  todos: firestore.data.todos,
+  requesting: firestore.status.requesting,
+  requested: firestore.status.requested
+});
+
+const mapDispatchToProps = {};
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  firestoreConnect(props => [`todos/${props.userId}`])
+)(Todos);
