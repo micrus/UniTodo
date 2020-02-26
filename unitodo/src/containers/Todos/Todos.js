6da6d2acfaec334e9897fcf014e1ad6c03b98151
luca-fuligni <luca.fuligni@studenti.unicam.it>
2020-02-26 15:00:23 +0100

    implementata opzione di aggiunta todo

--- unitodo/src/containers/Todos/Todos.js
+++ unitodo/src/containers/Todos/Todos.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from "styled-components";
+
+import Heading from "../../components/UI/Headings/Heading";
+import { Container } from "../../hoc/layout/elements";
+import AddTodo from './AddTodo/AddTodo';
+
+
+const Wrapper = styled.div`
+  width: 100%;
+  align-self: flex-start;
+  height: 100%;
+  min-height: calc(100vh - 6rem);
+  background-color: var(--color-mainLight);
+`;
+
+const InnerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5rem 4rem;
+`;
 
 const Todos = () => {
-  return <div>Qui andremo a mettere i nostri todo</div>;
+  return (
+    <Wrapper>
+      <Container>
+        <InnerWrapper>
+          <Heading noMargin size="h1" color="white">
+            I tuoi Todo
+          </Heading>
+          <Heading bold size="h4" color="white">
+            Tutto quello che devi fare...
+          </Heading>
+          <AddTodo />
+        </InnerWrapper>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Todos;
--- unitodo/src/store/reducers/index.js
+++ unitodo/src/store/reducers/index.js
@@ -1,12 +1,15 @@
 import { combineReducers } from "redux";
 
 import authReducer from "./authReducer";
+import todosReducer from './todosReducer';
+
 import { firebaseReducer } from "react-redux-firebase";
 import { firestoreReducer } from "redux-firestore";
 
 
 export default combineReducers({
   auth: authReducer,
+  todos: todosReducer,
   firebase: firebaseReducer,
   firestore: firestoreReducer
 });
