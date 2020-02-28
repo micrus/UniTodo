10c46dce71bff073e3d7b55ce70eed51af88dd75
luca-fuligni <luca.fuligni@studenti.unicam.it>
2020-02-28 17:19:39 +0100

    Updates

---
+++ precache-manifest.8fc4b8a3c38c28143be7faeccc70b9b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a399e092c4f6ba6ca61043790d85626",
+    "url": "/UniTodo/index.html"
+  },
+  {
+    "revision": "b0e890650e42bdc86280",
+    "url": "/UniTodo/static/js/2.9f154e53.chunk.js"
+  },
+  {
+    "revision": "4c86fd10a56eb60a384cd2f2c8426c35",
+    "url": "/UniTodo/static/js/2.9f154e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d8e88f64039845fe9ae",
+    "url": "/UniTodo/static/js/main.5d4d01ce.chunk.js"
+  },
+  {
+    "revision": "37d785b1de9ac7eb6fb7",
+    "url": "/UniTodo/static/js/runtime-main.0cf79174.js"
+  }
+]);
