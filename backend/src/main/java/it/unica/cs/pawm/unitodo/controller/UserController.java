32b120a0847e5043baca563a2cd59d5d8e06a331
micrus <michele.russo@studenti.unicam.it>
2020-02-29 15:27:35 +0100

    autorizzata cross origin react

--- backend/src/main/java/it/unica/cs/pawm/unitodo/controller/UserController.java
+++ backend/src/main/java/it/unica/cs/pawm/unitodo/controller/UserController.java
@@ -1,5 +1,27 @@
 package it.unica.cs.pawm.unitodo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import it.unica.cs.pawm.unitodo.model.User;
+import it.unica.cs.pawm.unitodo.service.UserService;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@Controller
 public class UserController {
+@Autowired
+private UserService userService;
+
+@PostMapping
+public User createUser(@RequestBody User user) {
+	
+	return userService.addUser(user);
+}
+
+
+
 
 }
