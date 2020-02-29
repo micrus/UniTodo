7ee2eaef3f3fd61309a1b788784734b918556bc0
micrus <michele.russo@studenti.unicam.it>
2020-02-29 11:00:21 +0100

    Aggiunto SpringInitializr

---
+++ backend/src/main/java/it/unica/cs/pawm/unitodo/UnitodoApplication.java
@@ -0,0 +1,13 @@
+package it.unica.cs.pawm.unitodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitodoApplication.class, args);
+	}
+
+}
